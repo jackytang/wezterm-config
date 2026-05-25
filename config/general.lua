fd79362e889dcc7984dcad862e5d82ad972babdf
JackyTang <jackytang01@gmail.com>
2026-05-25 15:28:50 +0800

    feat: enable kitty keyboard protocol

--- config/general.lua
+++ config/general.lua
@@ -1,6 +1,7 @@
 return {
    -- behaviours
    automatically_reload_config = true,
+   enable_kitty_keyboard = true,
    exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
    exit_behavior_messaging = 'Verbose',
    status_update_interval = 1000,
