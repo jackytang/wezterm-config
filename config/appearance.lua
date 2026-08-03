4f5641834f5bb61c3f52c3cca1fed397efba9f07
JackyTang <jackytang01@gmail.com>
2026-08-03 22:11:41 +0800

    update 更新刷新率

--- config/appearance.lua
+++ config/appearance.lua
@@ -4,7 +4,7 @@ local colors = require('colors.custom')
 
 ---@type Config
 return {
-   max_fps = 120,
+   max_fps = 160,
    front_end = 'WebGpu', ---@type 'WebGpu' | 'OpenGL' | 'Software'
    webgpu_power_preference = 'HighPerformance',
    webgpu_preferred_adapter = gpu_adapters:pick_best(),
